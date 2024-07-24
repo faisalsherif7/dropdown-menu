ebcbedd7cb631ef96fc21e8f72d45a179820c5a4
Faisal Sherif <116718026+faisalsherif7@users.noreply.github.com>
2024-07-24 18:33:47 +0530

    Create and style dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+
+const dropdownSelector = document.querySelector('.dropdown-selector')
+const dropdownItems = document.querySelector('.dropdown-items')
+
+dropdownItems.style.display = 'none'
+dropdownSelector.classList.add('invisible')
+
+function Dropdown(dropdownButton, dropdownItems) {
+    dropdownButton.addEventListener('click', (event) => {
+        if (dropdownButton.classList.contains('invisible')) {
+            dropdownButton.classList.remove('invisible')
+            dropdownItems.style.display = 'block'
+        } else {
+            dropdownButton.classList.add('invisible')
+            dropdownItems.style.display = 'none'
+        }
+    })
+}
+
+Dropdown(dropdownSelector, dropdownItems)
