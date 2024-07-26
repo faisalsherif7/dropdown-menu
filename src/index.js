6ded2623de114408810f01b0bafe2113ba4766e3
Faisal Sherif <116718026+faisalsherif7@users.noreply.github.com>
2024-07-26 16:01:53 +0530

    Separate event listeners from image navigation functionality

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ function intializeDropdowns() {
     })
 }
 
+function initializeEventListeners() {
+    const nextImageButton = document.querySelector('.next-image-button')
+    nextImageButton.addEventListener('click', () => {
+        chooseNextImage()
+    })
+    const previousImageButton = document.querySelector('.previous-image-button')
+    previousImageButton.addEventListener('click', () => {
+        choosePreviousImage()
+    })
+}
+
 function hideRestOfImageCarousel() {
     document.querySelectorAll('.image').forEach((imageElement) => {
         if (!imageElement.classList.contains('visible-image')) {
@@ -17,55 +28,52 @@ function hideRestOfImageCarousel() {
 }
 
 function choosePreviousImage() {
-    const previousImageButton = document.querySelector('.previous-image-button')
-    previousImageButton.addEventListener('click', () => {
-        const currentImageElement = document.querySelector('.visible-image')
-        const previousElement = currentImageElement.previousElementSibling
-        var previousImageElement
-        if (previousElement === null) {
-            previousImageElement =
-                currentImageElement.parentNode.lastElementChild
-        } else {
-            previousImageElement = previousElement
-        }
-        currentImageElement.classList.remove('visible-image')
-        currentImageElement.style.display = 'none'
-        previousImageElement.classList.add('visible-image')
-        previousImageElement.style.display = 'block'
-
-        updateNavigationDot()
-    })
+    const currentImageElement = document.querySelector('.visible-image')
+    const previousElement = currentImageElement.previousElementSibling
+    var previousImageElement
+    if (previousElement === null) {
+        previousImageElement = currentImageElement.parentNode.lastElementChild
+    } else {
+        previousImageElement = previousElement
+    }
+    currentImageElement.classList.remove('visible-image')
+    currentImageElement.style.display = 'none'
+    previousImageElement.classList.add('visible-image')
+    previousImageElement.style.display = 'block'
+    updateNavigationDot()
 }
 
 function chooseNextImage() {
-    const nextImageButton = document.querySelector('.next-image-button')
-    nextImageButton.addEventListener('click', () => {
-        const currentImageElement = document.querySelector('.visible-image')
-        const nextElement = currentImageElement.nextElementSibling
-        var nextImageElement
-        if (nextElement === null) {
-            nextImageElement = currentImageElement.parentNode.firstElementChild
-        } else {
-            nextImageElement = nextElement
-        }
-        currentImageElement.classList.remove('visible-image')
-        currentImageElement.style.display = 'none'
-        nextImageElement.classList.add('visible-image')
-        nextImageElement.style.display = 'block'
-
-        updateNavigationDot()
-    })
+    const currentImageElement = document.querySelector('.visible-image')
+    const nextElement = currentImageElement.nextElementSibling
+    var nextImageElement
+    if (nextElement === null) {
+        nextImageElement = currentImageElement.parentNode.firstElementChild
+    } else {
+        nextImageElement = nextElement
+    }
+    currentImageElement.classList.remove('visible-image')
+    currentImageElement.style.display = 'none'
+    nextImageElement.classList.add('visible-image')
+    nextImageElement.style.display = 'block'
+    updateNavigationDot()
 }
 
 intializeDropdowns()
+initializeEventListeners()
 hideRestOfImageCarousel()
 choosePreviousImage()
 chooseNextImage()
 
 function updateNavigationDot() {
-    const currentImageValue = document.querySelector('.visible-image').getAttribute('data-value')
-    document.querySelector('.current-image-dot').classList.remove('current-image-dot')
-    document.querySelector(`[class="dot"][data-value="${currentImageValue}`).classList.add('current-image-dot')
+    const currentImageValue = document
+        .querySelector('.visible-image')
+        .getAttribute('data-value')
+    document
+        .querySelector('.current-image-dot')
+        .classList.remove('current-image-dot')
+    document
+        .querySelector(`[class="dot"][data-value="${currentImageValue}`)
+        .classList.add('current-image-dot')
     console.log('here')
 }
-
